252968472e246819359a6c5c44ba67fff9c437dd
Igor Fanticheli <joao.fantiheli@gmail.com>
2021-03-03 22:49:15 -0300

    Primeiro commit

---
+++ src/main/java/com/igor/fanticheli/praticandojpa/PraticandoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.igor.fanticheli.praticandojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoJpaApplication.class, args);
+	}
+
+}
